8f5eba0aa0317a7f5087248e28f516c123b1b57a
Gaia <59805090+Gaia-Discord@users.noreply.github.com>
2020-02-25 10:13:15 +0100

    Added Function

--- bot.py
+++ bot.py
@@ -18,24 +18,25 @@ reddit = praw.Reddit(client_id=client_id,
 sub = reddit.subreddit(target_sub)
 mod = sub.mod
 
-then = datetime(2020, 2, 29, 9, 30, 00) # Countdown Date [YEAR, MONTH, DAY, HOUR, MINUTE, SECOND] [PST Timezone]
+def content(t): # Countdown Content Function
+    now = datetime.now()
+    duration = t - now
+    time1 = duration.total_seconds()
+    day = round(time1 // (24 * 3600))
+    time1 = time1 % (24 * 3600)
+    hour = round(time1 // 3600)
+    time1 %= 3600
+    minutes = round((time1 // 60) + 1) # +1 because seconds are hidden
+    time1 %= 60
+    seconds = time1
+    content = '**[{}](#DAYS) DAYS** **[{}](#HOURS) HOURS** **[{}](#MINUTES) MINUTES**'.format(day, hour, minutes)
+    return content
+
+PAX = datetime(2020, 2, 29, 9, 30, 00) # Countdown Date [YEAR, MONTH, DAY, HOUR, MINUTE, SECOND] [PST Timezone]
 
 count = -2
 while count < -1:
-    now = datetime.now()
-    duration = then - now
-    timeS = duration.total_seconds()
-
-    day = round(timeS // (24 * 3600))
-    timeS = timeS % (24 * 3600)
-    hour = round(timeS // 3600)
-    timeS %= 3600
-    minutes = round(timeS // 60)
-    timeS %= 60
-    seconds = timeS
-    
-    new_content = '**[{}](#DAYS) DAYS** **[{}](#HOURS) HOURS** **[{}](#MINUTES) MINUTES**'.format(day, hour, minutes) # Sidebar Content for Countdown
-
-    mod.update(description='{}'.format(new_content)) # Update Sidebar
-
-    time.sleep(10) # Run every 10 seconds
+    content_1 = content(PAX)
+    print(content_1)
+    mod.update(description='{}'.format(content_1)) # Update Sidebar
+    time.sleep(10) # Repeat Every 10 Seconds
